f694c119b61980ccec35f691a2fa55b21f613596
chrisseto <chriskseto@gmail.com>
2013-10-20 14:59:39 -0400

    Initial commit

---
+++ Lazy Manga Pirate/Form1.Designer.cs
@@ -0,0 +1,362 @@
+﻿namespace Lazy_Manga_Pirate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.verticalProgressBar1 = new Lazy_Manga_Pirate.VerticalProgressBar();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.Chapter = new System.Windows.Forms.ColumnHeader();
+            this.Pages = new System.Windows.Forms.ColumnHeader();
+            this.Progress = new System.Windows.Forms.ColumnHeader();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.tabPage4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(300, 436);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.progressBar1);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.listBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(292, 410);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Pick Ye\' Manga";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 356);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Search:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(58, 353);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(224, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(156, 379);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(126, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 379);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(147, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Refresh";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(8, 6);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.ScrollAlwaysVisible = true;
+            this.listBox1.Size = new System.Drawing.Size(274, 342);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.verticalProgressBar1);
+            this.tabPage2.Controls.Add(this.button3);
+            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.Controls.Add(this.checkedListBox1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(292, 410);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Pick Ye\' Chapters";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // verticalProgressBar1
+            // 
+            this.verticalProgressBar1.Location = new System.Drawing.Point(264, 6);
+            this.verticalProgressBar1.Name = "verticalProgressBar1";
+            this.verticalProgressBar1.Size = new System.Drawing.Size(20, 396);
+            this.verticalProgressBar1.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(8, 379);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(250, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Download Selected";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(8, 350);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(250, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Select All";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(8, 6);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(250, 334);
+            this.checkedListBox1.TabIndex = 0;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.label3);
+            this.tabPage3.Controls.Add(this.textBox2);
+            this.tabPage3.Controls.Add(this.label1);
+            this.tabPage3.Controls.Add(this.numericUpDown1);
+            this.tabPage3.Controls.Add(this.groupBox1);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(292, 410);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Options";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Save Location:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(93, 32);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(142, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.textBox2_MouseDoubleClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Number of Threads:";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(115, 6);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Location = new System.Drawing.Point(8, 317);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(276, 85);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "About";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 20);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(212, 52);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Created by Scruppy\r\nv. 1.1.0 8/6/2013\r\nIf you have any comments or issues please " +
+                "\r\nsend them to scruppy.tumblr.com";
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Controls.Add(this.listView1);
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(292, 410);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Jobs";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Chapter,
+            this.Pages,
+            this.Progress});
+            this.listView1.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.listView1.Location = new System.Drawing.Point(8, 6);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(276, 396);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // Chapter
+            // 
+            this.Chapter.Text = "Chapter";
+            this.Chapter.Width = 116;
+            // 
+            // Pages
+            // 
+            this.Pages.Text = "Pages";
+            this.Pages.Width = 58;
+            // 
+            // Progress
+            // 
+            this.Progress.Text = "Progress";
+            this.Progress.Width = 98;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(300, 436);
+            this.Controls.Add(this.tabControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Lazy Manga Pirate";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.tabPage4.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TabPage tabPage3;
+        private VerticalProgressBar verticalProgressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader Chapter;
+        private System.Windows.Forms.ColumnHeader Pages;
+        private System.Windows.Forms.ColumnHeader Progress;
+    }
+}
+
---
+++ Lazy Manga Pirate/Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Threading;
+using System.IO;
+using System.IO.Packaging;
+using System.Text.RegularExpressions;
+
+//NOTES:
+//everything formatted as http://www.goodmanga.net/{Manga Name}/chapter/{Chapter}/{Page Number}
+//Lies http://r1.goodmanga.net/images/manga/{Manga Name}/{Chapter}/{Page Number}.jpg
+//Prolly just gonna look for 404 for end of chapter :s
+
+namespace Lazy_Manga_Pirate
+{
+    public partial class Form1 : Form
+    {
+        HttpWebRequest client;
+        List<string> titles;
+        public Form1()
+        {
+            InitializeComponent();
+            textBox2.Text = Directory.GetCurrentDirectory() + "/";
+            titles = new List<string>();
+
+        }
+        #region getMangas
+        private void button1_Click(object sender, EventArgs e)
+        {
+            updateProgressBar(0);
+            client = ((HttpWebRequest)WebRequest.Create("http://www.goodmanga.net/manga-list"));
+            updateProgressBar(5);
+            client.BeginGetResponse(new AsyncCallback(listReceived), client);
+
+        }
+        private void listReceived(IAsyncResult result)
+        {
+            HttpWebResponse response = (result.AsyncState as HttpWebRequest).EndGetResponse(result) as HttpWebResponse;
+            String toParse = new StreamReader(response.GetResponseStream()).ReadToEnd();
+            response.Close();
+            updateProgressBar(50);
+            MatchCollection test = Regex.Matches(toParse, "<td><a href=\"http://www.goodmanga.net/.*\\/.*\">(?<Title>.*)<\\/a></td>", RegexOptions.IgnoreCase);
+            foreach (Match m in test)
+            {
+                titles.Add(m.Groups["Title"].ToString());
+            }
+            populateListBox(titles.ToArray());
+            updateProgressBar(100);
+
+        }
+        #endregion
+        #region UIStuff
+        private delegate void populateListBoxCallBack(string[] items);
+        private delegate void updateProgressBarCallBack(int percent);
+        private delegate void updateChaptersBoxCallBack(string[] chapters);
+
+        private void populateListBox(string[] items)
+        {
+            if (this.listBox1.InvokeRequired)
+            {
+                populateListBoxCallBack c = new populateListBoxCallBack(populateListBox);
+                this.Invoke(c, new object[] { items });
+            }
+            else
+            {
+                listBox1.Items.Clear();
+                listBox1.Items.AddRange(items);
+            }
+        }
+        private void updateProgressBar(int percent)
+        {
+            if (this.progressBar1.InvokeRequired)
+            {
+                updateProgressBarCallBack c = new updateProgressBarCallBack(updateProgressBar);
+                this.Invoke(c, new object[] { percent });
+            }
+            else
+            {
+                progressBar1.Value = percent;
+            }
+        }
+        private void updateChaptersBox(string[] chapters)
+        {
+            if (checkedListBox1.InvokeRequired)
+            {
+                updateChaptersBoxCallBack c = new updateChaptersBoxCallBack(updateChaptersBox);
+                this.Invoke(c, new object[] { chapters });
+            }
+            else
+            {
+                checkedListBox1.Items.Clear();
+                checkedListBox1.Items.AddRange(chapters);
+            }
+        }
+        private void updateVProgressBar()
+        {
+            if (verticalProgressBar1.InvokeRequired)
+            {
+                this.Invoke((MethodInvoker)delegate { updateVProgressBar(); });
+            }
+            else
+            {
+                verticalProgressBar1.Value++;
+                if (verticalProgressBar1.Value == verticalProgressBar1.Maximum)
+                {
+                    button2.Enabled = true;
+                    button3.Enabled = true;
+                    checkedListBox1.Enabled = true;
+                }
+            }
+        }
+        #endregion
+        #region getChapters
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                string chapterfetchuri = "http://www.goodmanga.net/" + formatMangaTitle(listBox1.SelectedItem.ToString()) + "/chapter/1";
+                client = ((HttpWebRequest)WebRequest.Create(chapterfetchuri));
+                client.BeginGetResponse(new AsyncCallback(fetchChapters), client);
+            }
+        }
+        private void fetchChapters(IAsyncResult result)
+        {
+            HttpWebResponse response = (result.AsyncState as HttpWebRequest).EndGetResponse(result) as HttpWebResponse;
+            string html = new StreamReader(response.GetResponseStream()).ReadToEnd();
+            List<string> chpts =  new List<string>();
+            foreach (Match m in Regex.Matches(html, "option v.+?\">(.+?(Chapter \\d{1,5}\\.\\d|Chapter \\d{1,5}))"))
+            {
+                chpts.Add(m.Groups[1].ToString());
+            }           
+            updateChaptersBox(chpts.ToArray());
+            if (tabControl1.InvokeRequired)
+            {
+                this.Invoke((MethodInvoker)delegate { tabControl1.SelectedTab = tabControl1.TabPages[1]; });
+            }
+            else
+            {
+                tabControl1.SelectedTab = tabControl1.TabPages[1];
+            }
+
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Directory.CreateDirectory(listBox1.SelectedItem.ToString() + "/");
+            string chapterurl = "http://t1.goodmanga.net/images/manga/" + formatMangaTitle(listBox1.SelectedItem.ToString()) + "/";
+            verticalProgressBar1.Maximum = checkedListBox1.CheckedItems.Count;
+            verticalProgressBar1.Value = 0;
+            button2.Enabled = false;
+            button3.Enabled = false; ;
+            checkedListBox1.Enabled = false;
+            ThreadPool.SetMaxThreads((int)numericUpDown1.Value, (int)numericUpDown1.Value);
+            for (int i = 0; i < checkedListBox1.CheckedItems.Count; i++)
+            {
+                string temp = checkedListBox1.CheckedItems[i].ToString().Trim();
+                temp = temp.Substring(temp.LastIndexOf(' ') + 1);
+                ThreadPool.QueueUserWorkItem(new WaitCallback(downloadChapter), new object[] { chapterurl + temp + "/", listBox1.SelectedItem.ToString(), temp });
+            }
+        }
+        private void downloadChapter(object param1)
+        {
+            object[] param = (object[])param1;
+            string uri = (string)param[0];
+            string manga = (string)param[1];
+            int chapterNumber = Convert.ToInt32((string)param[2]);
+            int page = 1;
+            if (!File.Exists(textBox2.Text + manga + "/" + manga + " Chapter " + chapterNumber.ToString("000") + ".cbz"))
+            {
+                using (Package zip = Package.Open(textBox2.Text + manga + "/" + manga + " Chapter " + chapterNumber.ToString("000") + ".cbz", FileMode.OpenOrCreate))
+                {
+                    try
+                    {
+                        while (true)
+                        {
+                            WebRequest dl = WebRequest.Create(uri + page + ".jpg");
+                            WebResponse res = dl.GetResponse();
+                            string temp = page.ToString("000") + ".png";
+                            Uri pageUri = PackUriHelper.CreatePartUri(new Uri(page.ToString("000") + ".png", UriKind.Relative));
+                            PackagePart part = zip.CreatePart(pageUri, "", CompressionOption.Normal);
+                            Image.FromStream(res.GetResponseStream()).Save(part.GetStream(), System.Drawing.Imaging.ImageFormat.Png);
+                            res.Close();
+                            page++;
+                        }
+                    }
+                    catch
+                    {
+                    }
+                }
+                updateVProgressBar();
+            }
+            //Might add error handling later....
+            //Might not
+        }
+        #endregion
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < checkedListBox1.Items.Count; i++)
+                checkedListBox1.SetItemChecked(i, true);
+        }
+        private string formatMangaTitle(string unformated)
+        {
+            if (unformated.Contains('/') || unformated.Contains('.') || unformated.Contains('+') || unformated.Contains('[') || unformated.Contains('%') || unformated.Contains('('))
+            {
+                return unformated.ToLower().Trim().Replace(' ', '-').Replace(".", "").Replace("/", "").Replace("+", "").Replace("\\", "").Replace("[", "").Replace("]", "");
+            }
+            else
+            {
+                return unformated.ToLower().Trim().Replace(' ', '_');
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            foreach (string s in titles)
+            {
+                if (s.ToString().ToLower().Contains(((TextBox)sender).Text.ToLower()))
+                {
+                    listBox1.Items.Add(s);
+                }
+            }
+        }
+
+        private void textBox2_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            if(Directory.Exists(folderBrowserDialog1.SelectedPath))
+                textBox2.Text = folderBrowserDialog1.SelectedPath;
+        }
+    }
+}
